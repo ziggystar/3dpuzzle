600e1a74c234b0601e37fc1130f1d59c7e765995
Thomas Geier <thomas.geier@uni-ulm.de>
2015-05-21 14:54:58 +0200

    use piece numbers from file

--- src/main/scala/puzzle2d/gui/Main.scala
+++ src/main/scala/puzzle2d/gui/Main.scala
@@ -7,32 +7,33 @@ import puzzle2d._
 import spray.json._
 import util.gui.{RXButton, MigPanel}
 
-import scala.collection.immutable.Map
 import scala.io.Source
 import scala.swing._
 
 object Main {
   object PuzzleJsonProtocol extends DefaultJsonProtocol {
-
-    implicit object JsonPiece extends RootJsonFormat[Piece] {
+    case class NumberedPiece(piece: Piece, count: Int)
+    implicit object JsonPiece extends JsonFormat[Piece] {
       override def read(json: JsValue): Piece = json match {
-        case JsObject(fields) if fields.contains("piece") =>
-          fields("piece") match {
-            case JsString(desc) => new Piece(Shape.parseString(desc, '#'))
-          }
+        case JsString(desc) => new Piece(Shape.parseString(desc, '#'))
       }
-
-      override def write(p: Piece): JsValue = JsObject("piece" -> JsString(p.representative.toString))
+      override def write(p: Piece): JsValue = JsString(p.representative.toString)
     }
 
-  }
-  import PuzzleJsonProtocol._
+    implicit val jsonPieceSet: RootJsonFormat[NumberedPiece] = jsonFormat2(NumberedPiece)
 
-  def fromJson(js: JsValue): PieceSet = js match {
-    case JsObject(fields) => PieceSet(fields("piece-set").convertTo[Seq[Piece]].zip(Stream.continually(10)).toMap)
+    implicit object PieceSetJs extends RootJsonFormat[PieceSet]{
+      override def write(obj: PieceSet): JsValue = JsArray(obj.pieces.toVector.map{case (p,i) => NumberedPiece(p,i).toJson})
+
+      override def read(json: JsValue): PieceSet = json match {
+        case JsArray(nps) => PieceSet(nps.map(_.convertTo[NumberedPiece]).map(np => np.piece  -> np.count)(collection.breakOut))
+      }
+    }
   }
+
   val pieceSet = {
-    fromJson(JsonParser(Source.fromInputStream(ClassLoader.getSystemResourceAsStream("puzzle2d/pieceset-kubix.json")).getLines().mkString("\n")))
+    import PuzzleJsonProtocol._
+    JsonParser(Source.fromInputStream(ClassLoader.getSystemResourceAsStream("puzzle2d/pieceset-kubix.json")).getLines().mkString("\n")).asJsObject.fields("piece-set").convertTo[PieceSet]
   }
 
   def main (args: Array[String] ) {
